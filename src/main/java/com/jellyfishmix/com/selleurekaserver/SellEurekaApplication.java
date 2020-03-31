aa748aab9769600172ec96b066e14ded93a19994
JellyfishMIX <2273516339@qq.com>
2020-03-31 21:30:37 +0800

    :construction: Change the package name from "sell-eureka" to "sell-eureka-server"

--- src/main/java/com/jellyfishmix/com/selleurekaserver/SellEurekaApplication.java
+++ src/main/java/com/jellyfishmix/com/selleurekaserver/SellEurekaApplication.java
@@ -1,4 +1,4 @@
-package com.jellyfishmix.com.selleureka;
+package com.jellyfishmix.com.selleurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/test/java/com/jellyfishmix/com/selleurekaserver/SellEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jellyfishmix.com.selleurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
